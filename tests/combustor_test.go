9168f78165b364f5fb1ddb68b2536dd9f6f30bc8
Alexander Krasnoschekov <akrasnoschekov@gmail.com>
2016-10-20 19:16:15 +0300

    restructures test files

---
+++ tests/combustor_test.go
@@ -0,0 +1,5 @@
+package tests
+
+// check that combustor instantiates stored messages
+// check that combustor discards unnecessary ones
+// check that combustor decreases span when messages are sent
---
+++ tests/turbine_test.go
@@ -0,0 +1,5 @@
+package tests
+
+// check that turbine persists dirty messages
+// check that turbine requeues messages from dead connections
+// check that turbine removes sent messages
