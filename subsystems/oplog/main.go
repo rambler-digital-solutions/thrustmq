ddb1e296fde89ef94f0e49e52d829de244bff129
Alexander Krasnoschekov <akrasnoschekov@gmail.com>
2016-11-22 15:11:10 +0300

    splits logs into several files

--- subsystems/oplog/main.go
+++ subsystems/oplog/main.go
@@ -2,6 +2,7 @@ package oplog
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/rambler-digital-solutions/thrustmq/common"
 	"github.com/rambler-digital-solutions/thrustmq/config"
 	"log"
@@ -28,17 +29,32 @@ func dashboardHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func Init() {
-	go logger()
+	go writeLogs()
 	http.HandleFunc("/dash", dashboardHandler)
 	http.ListenAndServe(":3888", nil)
 }
 
-func logger() {
-	logfile, err := os.OpenFile(config.Base.Logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+func getLog(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	common.FaceIt(err)
 	logger := log.New(logfile, "", log.LstdFlags)
-	logger.Printf("\n\n\n\n Oplog started.")
+	logger.Printf("\n\n\n\n Started.")
+	return logger
+}
 
+func getLogFromMap(logMap map[string]*log.Logger, subsystem string) *log.Logger {
+	value := logMap[subsystem]
+	if value == nil {
+		filename := fmt.Sprintf("%s_%s.log", config.Base.Logfile, subsystem)
+		fmt.Printf(filename)
+		logMap[subsystem] = getLog(filename)
+	}
+	return logMap[subsystem]
+}
+
+func writeLogs() {
+	logger := getLog(config.Base.Logfile + ".log")
+	logMap := make(map[string]*log.Logger)
 	var prev = common.OplogRecord{}
 	var current common.OplogRecord
 	var counter = 1
@@ -46,6 +62,7 @@ func logger() {
 		current = <-common.OplogChannel
 		if current.Message != prev.Message {
 			logger.Printf("[%.4s] %s", strings.ToUpper(current.Subsystem), current.Message)
+			getLogFromMap(logMap, current.Subsystem).Printf(current.Message)
 			counter = 1
 			prev = current
 		} else {
@@ -54,6 +71,5 @@ func logger() {
 				logger.Printf("... repeats")
 			}
 		}
-
 	}
 }
